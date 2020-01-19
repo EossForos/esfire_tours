1da1625941f40ac0edc1899764b9948de7395c57
EossForos <volosonopulos@gmail.com>
2020-01-19 23:16:40 +0200

    urls http post

--- esfire_tours/urls.py
+++ esfire_tours/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('general.urls')),
+    path('esfire-tours.herokuapp.com', include('general.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
